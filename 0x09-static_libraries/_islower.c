65803a7b00571df8129d5d1d3d98892877978cbc
Hamdyyat <alhassanhamdy1@gmail.com>
2023-08-03 14:29:25 -0700

    _islower(int c)

---
+++ 0x09-static_libraries/_islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower(int c) - uppercase letters
+ * @c: char to check
+ */
+
+int _islower(int c)
+{
+    /* Implementation of _islower */
+    return (c >= 'a' && c <= 'z');
+}
+
